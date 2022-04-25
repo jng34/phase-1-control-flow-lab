297e1f6fa25f1ddd93a97bedff4b99e119cb4319
JamesNg <njames15@gmail.com>
2022-04-25 17:29:23 -0400

    commit tree c5b0120e84ce28b7731501abc43c61f21902539d

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(val){
   // Write your code here!
+  if (val <= 400) {
+    return 'This one is on me!';
+  } else if (val > 400 && val < 2000) {
+    return 'That will be twenty bucks.';
+  } else if (val > 2000 && val < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (val > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(lvl){
   // Write your code here!
+  if (lvl === 'generous') {
+    return 'Thank you so much.';
+  } else if (lvl === 'not as generous') {
+    return 'Thank you.';
+  } else if (lvl === 'thanks for everything') {
+    return 'Bye.'
+  }
 }
